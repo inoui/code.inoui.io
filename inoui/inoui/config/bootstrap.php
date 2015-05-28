87ccbd5cd83ccc53bb4c102e23992eee11045b6d
Thomas Goetz <tom@inoui.io>
2015-05-28 12:02:51 +0200

    home class

--- inoui/inoui/config/bootstrap.php
+++ inoui/inoui/config/bootstrap.php
@@ -8,15 +8,19 @@ Libraries::add('li3_mailer');
 use li3_mailer\net\mail\Delivery;
 use li3_mailer\action\Mailer;
 
-Libraries::add('swiftmailer', array( 'bootstrap' => 'lib/swift_required.php'));
-Libraries::add("Zend", array(
-    "prefix" => "Zend_",
-    'path' => LITHIUM_LIBRARY_PATH . '/Zend/library/Zend',
-    "includePath" => LITHIUM_LIBRARY_PATH . '/Zend/library',
-    "bootstrap" => "Loader/Autoloader.php",
-    "loader" => array("Zend_Loader_Autoloader", "autoload"),
-    "transform" => function($class) { return str_replace("_", "/", $class) . ".php"; }
+Libraries::add('swiftmailer', array( 
+    'path' => LITHIUM_LIBRARY_PATH . '/swiftmailer/swiftmailer',
+    'bootstrap' => 'lib/swift_required.php'
 ));
+
+// Libraries::add("Zend", array(
+//     "prefix" => "Zend_",
+//     'path' => LITHIUM_LIBRARY_PATH . '/Zend/library/Zend',
+//     "includePath" => LITHIUM_LIBRARY_PATH . '/Zend/library',
+//     "bootstrap" => "Loader/Autoloader.php",
+//     "loader" => array("Zend_Loader_Autoloader", "autoload"),
+//     "transform" => function($class) { return str_replace("_", "/", $class) . ".php"; }
+// ));
 
 require __DIR__ . '/bootstrap/media.php';
 
